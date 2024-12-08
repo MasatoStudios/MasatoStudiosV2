27675940a3ccd170f68106004aebca8bfc7f5f3a
Matthew Storey <mattstorey@hotmail.co.uk>
2024-12-08 17:54:39 +0000

    added gradient background, tidy file structure, fixed navbar

--- src/app/Components/Navigation/index.jsx
+++ src/app/Components/Navigation/index.jsx
@@ -1,26 +1,35 @@
+// imports
 import React from 'react'
-import './navbar.module.css' // Make sure to create a CSS file for styling
+import Link from 'next/link'
+import { clsx } from 'clsx'
+
+// styles
+import styles from './navbar.module.css'
+
+// TODO: Add styles (maybe the language selector)
+// Add Font, style text and links when active, add hover effect
 
 const Navbar = () => {
 	return (
-		<nav className='navbar'>
-			<div className='navbar-logo'>
-				<img src='/path/to/your/logo.png' alt='Logo' />
+		<nav className={styles.navContainer}>
+			<div className={styles.navNameContainer}>
+				<Link className={clsx(styles.link)} href='/'>
+					<span className={styles.navNameFullSpan}>
+						<span className={styles.navNameFirst}>Masato</span>
+						<span className={styles.navNameSecond}>Studios</span>
+					</span>
+				</Link>
 			</div>
-			<div className='navbar-menu'>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					viewBox='0 0 24 24'
-					width='24'
-					height='24'
-				>
-					<path
-						d='M3 6h18M3 12h18M3 18h18'
-						stroke='currentColor'
-						strokeWidth='2'
-						strokeLinecap='round'
-					/>
-				</svg>
+			<div className={styles.NavLinksContainer}>
+				<Link className={clsx(styles.link)} href='/about'>
+					About
+				</Link>
+				<Link className={clsx(styles.link)} href='/projects'>
+					Projects
+				</Link>
+				<Link className={clsx(styles.link)} href='/contact'>
+					Contact
+				</Link>
 			</div>
 		</nav>
 	)
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-    <div><p>Hello World</p></div>
-  );
+	const [gradient, setGradient] = useState('')
+
+	useEffect(() => {
+		// Define a list of colors
+		const colors = [
+			'#ff9a9e',
+			'#fad0c4',
+			'#fbc2eb',
+			'#a18cd1',
+			'#84fab0',
+			'#8fd3f4',
+		]
+
+		const generateGradient = () => {
+			const randomColor1 = colors[Math.floor(Math.random() * colors.length)]
+			const randomColor2 = colors[Math.floor(Math.random() * colors.length)]
+			return `linear-gradient(to right, ${randomColor1}, ${randomColor2})`
+		}
+
+		setGradient(generateGradient())
+
+		const interval = setInterval(() => {
+			setGradient(generateGradient())
+		}, 5000)
+
+		return () => clearInterval(interval)
+	}, [])
+
+	return (
+		<div>
+			<p>Hello World</p>
+		</div>
+	)
 }
